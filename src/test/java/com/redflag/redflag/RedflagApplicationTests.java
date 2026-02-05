ee88b3f9a4e40b781a88a6de2e659ad59d33a7f1
jiwonniy <jiwoni0707@gmail.com>
2026-02-05 11:46:57 +0900

    init : project setup

---
+++ src/test/java/com/redflag/redflag/RedflagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redflag.redflag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedflagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
